eccbacebf5daa1f05f022e123b845dc7e39b1bd4
shema yvan <syvan317@gmail.com>
2026-05-27 19:51:24 +0200

    Completed navbar routing and linked pages

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,11 @@
 <script setup>
+import { RouterView } from 'vue-router';
+
+// import home from './Views/home.vue'
+ import navbar from './Components/navbar.vue'
 </script>
-
 <template>
-  <div class="bg-gray-100 min-h-screen">
-
-    
-    <header class="bg-sky-600 text-white text-center ">
-      <h1 class="font-bold">SOS Technical High School</h1>
-      <p class="text-sm mt-1">Learning Technology, Building the Future</p>
-    </header>
-
-    <section class="max-w-5xl mx-auto p-6 md:flex items-center gap-6">
-
-      <img  src="./Assets/soslogo2.png"alt="school logo"class="w-full md:w-1/2 rounded-lg"/>
-
-      <div class="mt-4 md:mt-0">
-        <h2 class="text-xl font-semibold text-gray-800">
-          Welcome to our school
-        </h2>
-
-        <p class="mt-3 text-gray-600">
-          SOS Technical High School offers both academic and technical education.
-          Students learn practical skills that help them prepare for future careers.
-        </p>
-      </div>
-
-    </section>
-
-  
-    <section class="max-w-5xl mx-auto px-6 py-6">
-
-      <h2 class="text-lg font-semibold text-gray-800 mb-4">
-        School Environment
-      </h2>
-
-      <img
-        src="./Assets/image.png" class="w-full rounded-lg shadow"/>
-
-      <p class="mt-3 text-gray-600 text-sm">
-        Students are actively involved in classroom activities and group learning.
-        Leading TVET Excellence in East Africa To be East Africa's most impactful TVET institution
-        , producing skilled graduates who drive innovation and economic growth.
-         We envision a future where technical education is the engine of Rwanda's development.
-      </p>
-
-    </section>
-
-
-    <section class="max-w-5xl mx-auto px-6 py-6 grid md:grid-cols-3 gap-4">
-
-      <div class="p-4 bg-white border rounded-lg">
-        <h3 class="font-bold text-blue-600">ICT Department</h3>
-        <p class="text-sm text-gray-600 mt-2">
-          We study programming, computer basics, and digital skills.
-        </p>
-      </div>
-
-      <div class="p-4 bg-white border rounded-lg">
-        <h3 class="font-bold text-blue-600">Practical Training</h3>
-        <p class="text-sm text-gray-600 mt-2">
-          Hands-on practice in technical subjects and lab work.
-        </p>
-      </div>
-
-      <div class="p-4 bg-white border rounded-lg">
-        <h3 class="font-bold text-blue-600">Student Growth</h3>
-        <p class="text-sm text-gray-600 mt-2">
-          Focus on discipline, teamwork, and personal development.
-        </p>
-      </div>
-
-    </section>
-
-  </div>
+  <navbar/>
+  <router-view/>
+<!-- <home/> -->
 </template>
-
-<style scoped>
-</style>
---
+++ src/Views/home.vue
@@ -0,0 +1,4 @@
+<template>
+  <p class="text-center text-blue-500 font-bold text-lg">TEMBERA URWANDA PROJECT</p>
+</template>
+
